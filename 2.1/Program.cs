3a7c5428dc1282776d1ac4b1a77cf4f5946c188e
Луценко Максим <lutsenkomaks20042007@gmail.com>
2024-11-11 19:24:54 +0200

    Add project files.

---
+++ 2.1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class GuessNumberGame
+{
+    static void Main()
+    {
+        Random random = new Random();
+        int targetNumber = random.Next(1, 11);
+        int userGuess;
+        bool guessedCorrectly = false;
+
+        Console.WriteLine("Гра 'Вгадай число'! Загадано число від 1 до 10.");
+
+        while (!guessedCorrectly)
+        {
+            Console.Write("Введіть ваше число (або 0 для завершення): ");
+            if (!int.TryParse(Console.ReadLine(), out userGuess) || userGuess < 0 || userGuess > 10)
+            {
+                Console.WriteLine("Некоректний ввід. Спробуйте знову.");
+                continue;
+            }
+
+            if (userGuess == 0)
+            {
+                Console.WriteLine("Гру завершено.");
+                break;
+            }
+
+            if (userGuess == targetNumber)
+            {
+                Console.WriteLine("Вітаємо! Ви вгадали число.");
+                guessedCorrectly = true;
+            }
+            else if (userGuess < targetNumber)
+            {
+                Console.WriteLine("Загадане число більше.");
+            }
+            else
+            {
+                Console.WriteLine("Загадане число менше.");
+            }
+        }
+    }
+}
